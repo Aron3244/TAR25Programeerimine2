2b4797b38394c4f99b1c3ea5757ec84a46132b70
Miilo2004 <48867030+Miilo2004@users.noreply.github.com>
2026-09-07 14:10:46 +0300

    SpaceshipDto/Database save

--- ShopTARpe25/ShopTARpe25.ApplicationServices/Services/SpaceshipServices.cs
+++ ShopTARpe25/ShopTARpe25.ApplicationServices/Services/SpaceshipServices.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ShopTARpe25.Core.Domain;
+using ShopTARpe25.Core.Dto;
+using ShopTARpe25.Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,34 @@ namespace ShopTARpe25.ApplicationServices.Services
 {
     public class SpaceshipServices
     {
+        private readonly ShopTARpe25Context _context;
+
+        public SpaceshipServices(ShopTARpe25Context context)
+        {
+            _context = context;
+        }
+        public async Task<Spaceship> Create(SpaceshipDto dto)
+        {
+            Spaceship domain = new();
+
+            domain.Id = dto.Id;
+            domain.Name = dto.Name;
+            domain.Classification = dto.Classification;
+            domain.BuiltDate = dto.BuiltDate;
+            domain.Crew = dto.Crew;
+            domain.EnginePower = dto.EnginePower;
+            domain.CreatedAt = dto.CreatedAt;
+            domain.ModifiedAt = dto.ModifiedAt;
+
+            //siia tuleb kood, mis salvestab domaini
+            //objekti andmebaasi
+            //tuleb kasutada repositoryd, mus on defineeritud Core projektis
+            //konstruktori kadu tuleb injectida repository
+            await _context.Spaceships.AddAsync(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+
+        }
     }
 }
