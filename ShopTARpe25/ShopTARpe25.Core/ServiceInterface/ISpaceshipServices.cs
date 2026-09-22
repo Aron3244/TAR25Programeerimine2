1568662b37c471b0f4bc5962e2adf1ea6b8cc793
Kundratsk <Aadupill05@gmail.com>
2026-09-22 14:07:47 +0300

    Deletevm

--- ShopTARpe25/ShopTARpe25.Core/ServiceInterface/ISpaceshipServices.cs
+++ ShopTARpe25/ShopTARpe25.Core/ServiceInterface/ISpaceshipServices.cs
@@ -8,6 +8,7 @@ namespace ShopTARpe25.Core.ServiceInterface
     {
         Task<Spaceship> Create(SpaceshipDto dto);
         Task<Spaceship> DetailsAsync(Guid id);
+        Task<Spaceship> Delete(Guid id);
         Task<Spaceship> Update(SpaceshipDto dto);
     }
 }
